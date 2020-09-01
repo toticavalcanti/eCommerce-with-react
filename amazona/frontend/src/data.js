e240e01c260d24d1b55421b57486213038094987
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-09-01 17:57:00 -0300

    render products

---
+++ amazona/frontend/src/data.js
@@ -0,0 +1,62 @@
+export default {
+  products: [
+    {
+      name: 'Slim Shirt',
+      category: 'Shirts',
+      image: '/images/slim-shirt-01.jpg',
+      price: 60.00,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+
+    {
+      name: 'Fit Shirt',
+      category: 'Shirts',
+      image: '/images/fit-shirt-01.jpg',
+      price: 50.00,
+      brand: 'Nike',
+      rating: 4.2,
+      numReviews: 5
+    },
+
+    {
+      name: 'Best Pants',
+      category: 'Pants',
+      image: '/images/pants-01.jpg',
+      price: 70.00,
+      brand: 'Nike',
+      rating: 4.0,
+      numReviews: 2
+    },
+    {
+      name: 'Slim Shirt',
+      category: 'Shirts',
+      image: '/images/slim-shirt-02.jpg',
+      price: 65.00,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+
+    {
+      name: 'Fit Shirt',
+      category: 'Shirts',
+      image: '/images/fit-shirt-02.jpg',
+      price: 55.00,
+      brand: 'Nike',
+      rating: 4.2,
+      numReviews: 5
+    },
+
+    {
+      name: 'Best Pants',
+      category: 'Pants',
+      image: '/images/pants-02.jpg',
+      price: 75.00,
+      brand: 'Nike',
+      rating: 4.0,
+      numReviews: 2
+    }
+  ]
+}
