503c0230a35a4d26ef2e4983c029bf78d90b4782
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-09-27 20:14:08 -0300

    ending of shopping cart screen

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-import CartScreen from './screens/Cart  Screen';
+import CartScreen from './screens/CartScreen';
 
 function App() {
 
